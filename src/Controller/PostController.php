b4ade985c5fcdfc7625dfb8418044886ba7cfb44
solofoniaina <solofoniainarakotoharimanana@gmail.com>
2024-02-09 15:42:13 +0300

    Feat: Install pagination

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,19 +3,32 @@
 namespace App\Controller;
 
 use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
     #[Route(path:"/", name:"post.index", methods: ["GET"])]
-    public function index(PostRepository $postRepository): Response
+    public function index(
+        PostRepository $postRepository,
+        PaginatorInterface $paginatorInterface,
+        Request $request
+        ): Response
     {
+
+        $datas = $postRepository->findPublished();
+        $posts = $paginatorInterface->paginate(
+                        $datas, 
+                        $request->query->getInt("page",1),
+                        9
+                    );
 
         return $this->render("pages/blog/index.html.twig", 
             [
-                'posts' => $postRepository->findPublished()
+                'posts' => $posts
             ]
         );
     }
